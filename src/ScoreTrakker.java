999357f88200fa471cc37eb2beb1f87fa015e357
moiven <mruiz@mymail.mines.edu>
2015-09-25 22:46:36 -0600

    made changes to loadDataFromFile and processFiles to throw exceptions

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class ScoreTrakker {
 	private ArrayList<Student> students;
-	private static String[] files = {"scoresPt1.txt", "badscore.txt", "nofile.txt"};
+	private static String[] files = {"scoresPt1.txt", "badscore.txt", "nofile.txt", "badname.txt"};
 	
 	public ScoreTrakker()
 	{
@@ -21,14 +21,21 @@ public class ScoreTrakker {
 		while (in.hasNextLine()) 
 		{
 			Student kid = new Student();
-
-			String inputName = in.nextLine();
-			kid.setName(inputName);
 			
 			//modified to catch errors in student scores
 			String strNum = null;
+			String inputName = null;
 			try
 			{
+				inputName = in.nextLine();
+				//check if students are missing last names or first names
+				//checks for whitespace between names
+				if(inputName.indexOf(' ') == -1)
+				{
+					throw new Exception("\n'" + inputName + "' does not include a first and last name");
+				}
+				kid.setName(inputName);
+				
 				strNum = in.nextLine();
 				int inputScore = Integer.parseInt(strNum);
 				kid.setScore(inputScore);
@@ -49,6 +56,7 @@ public class ScoreTrakker {
 		{
 			System.out.println(stud.toString());
 		}
+		students.clear();
 	}
 	
 	public void processFiles(String[] filename) throws Exception
@@ -60,9 +68,16 @@ public class ScoreTrakker {
 				loadDataFromFile(file);
 				printInOrder();
 			}
+			//catch exception if there is no file found
 			catch (FileNotFoundException e)
 			{
 				System.out.println("\nCan't open file: " + file);
+			}
+			//added catch for the exception in loadDataFromFile
+			catch(Exception e)
+			{
+				System.out.println(e.getMessage());
+				return;
 			}
 		}
 	}
